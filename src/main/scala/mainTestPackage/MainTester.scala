94241bfeb44198dc9ba076c6881a949326629238
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2022-01-24 22:51:35 +0200

    Version with comment before division into more functions

--- src/main/scala/mainTestPackage/MainTester.scala
+++ src/main/scala/mainTestPackage/MainTester.scala
@@ -9,6 +9,84 @@ import scala.collection.mutable.HashMap
 
 object MainTester {
 
+  /**
+   *  A function that implements the main Query Optimization logic.
+   * @param fullMap: Dictionary
+   * @param costMap
+   * @return
+   */
+  def QueryOptimizerLogic(fullMap: HashMap[String, Array[String]], costMap: HashMap[String, Double]): String ={
+
+    /*
+      For Q2
+      Tabs Analysis:
+       -  tab0 -> As X -> GraduateStudent ->    Cost: 1379623
+       -  tab1 -> As Y -> University  ->        Cost: 724685
+       -  tab2 -> As Z -> Department  ->        Cost: 309815
+       -  tab3 -> AS X, AS Z -> memberOf ->     Cost: 5580609
+       -  tab4 -> AS Z, AS Y -> suborganOf->    Cost: 160140
+       -  tab5 -> AS X, AS Y -> undergraDegree->Cost: 1619476
+     */
+
+    //    WHERE tab4.Z=tab2.Z AND tab1.Y=tab4.Y AND tab4.Y=tab5.Y AND tab3.Z=tab4.Z AND tab5.X=tab0.X AND tab0.X=tab3.X
+
+    //    We essentially want to find the Hamiltonial path  https://www.hackerearth.com/practice/algorithms/graphs/hamiltonian-path/tutorial/
+    //    When there are N nodes, there are N - 1 directed edges that can lead from it (going to every other node)
+    //  https://stackoverflow.com/questions/5058406/what-is-the-maximum-number-of-edges-in-a-directed-graph-with-n-nodes?fbclid=IwAR0EePHHzvtL1b0XtckIbjjaXhGaYG7HEgyNDEd5EMF34dKdIv9Oiz_zYok
+    //  PROOF: https://www.quora.com/How-do-I-prove-that-the-minimum-number-of-edges-in-a-connected-graph-with-n-vertices-is-n-1
+
+    /*
+//    Q2 data
+//    var costMap: HashMap[String, Double] = HashMap("tab0" -> 1379623, "tab1" -> 724685, "tab2" -> 309815, "tab3" -> 5580609, "tab4" -> 160140, "tab5" -> 1619476)
+//    var fullMap: HashMap[String, Array[String]] = HashMap("X"-> Array("tab0", "tab3", "tab5"), "Y"-> Array("tab1","tab4", "tab5"), "Z"->Array("tab2","tab3","tab4"))
+
+//    Q4 Data
+//    var costMap: HashMap[String, Double] = HashMap("tab0" -> 1379623, "tab1" -> 724685, "tab2" -> 309815, "tab3" -> 5580609, "tab4" -> 160140)
+//    var fullMap: HashMap[String, Array[String]] = HashMap("X"-> Array("tab0", "tab1", "tab2", "tab3", "tab4"), "Y1" -> Array("tab2"), "Y2" -> Array("tab3"), "Y3" -> Array("tab4"))
+*/
+
+    //TODO fullmap and costmap the most important here
+
+    var bestComb : List[(String, Double)] = Nil // A list to preserve the best combinations
+
+    //First iterate over the Hashmap (key,values). Values is a list that contains all the available tables between them
+    //We are certain that there are no joins where only one table of a specific key exists
+    for (dictIter <- fullMap; if dictIter._2.length > 1 ){
+      //      println("\n\nFor the key " + dictIter._1)
+
+      var tempComb : List[(String, Double)] = Nil
+      for(innerList <- dictIter._2.indices){  //  Iterate over the list elements. innerList is an index value
+        //        println("Iterate over list elements " +dictIter._2(innerList))
+        for(remElem <- innerList+1 until dictIter._2.length){   // Go from the next index to the end of the table to track all combinations
+          //          println("Remaining elements " +dictIter._2(remElem))
+
+          val tempProd: Double = costMap(dictIter._2(innerList)) * costMap(dictIter._2(remElem))  // Find the cost of join between two arrays, by calculating the product
+          //          println("\n\nCost of element "+ dictIter._2(innerList) +" is "+costMap(dictIter._2(innerList)))
+          //          println("Cost of element "+ dictIter._2(remElem) +" is "+costMap(dictIter._2(remElem)))
+          val tempTuple: (String, Double) = (dictIter._2(innerList)+"."+dictIter._1+"="+dictIter._2(remElem)+"."+dictIter._1, tempProd)  // Create a temporary tuple with the table name and the cost
+          //          println("Temporary tuple", tempTuple)
+          tempComb = tempTuple :: tempComb
+        }
+      }
+
+      //      println("List with all the combinations \t"+ tempComb)
+      val sortedtempComb : List[(String, Double)]= tempComb.sortBy(_._2)  //Sort everything by the join cost(ascending)
+      //      println("List with SORTED the combinations \t"+ sortedtempComb)
+
+      for(bestElem <- 0 until dictIter._2.length - 1){ //Keep the N - 1 elements that we want from each case. NOTE N-1 from the initial list with the elements
+        bestComb = sortedtempComb(bestElem) :: bestComb
+      }
+
+    }
+
+    val sortedBestComb : List[String] = bestComb.sortBy(_._2).map(x => x._1) //Sort once again to get the final ordering, and keep the only the combinations
+    val finalWhereQuery = " WHERE " +sortedBestComb.mkString(" AND ") //Final WHERE STATEMENT OPTIMIZED
+
+    //println("Final WHERE STATEMENT "+ finalWhereQuery) //WHERE sanity check
+
+    finalWhereQuery
+  }
+
   def QueryOptimizer(inputQuery: String) : String = {
 
     val newlineSplit: Array[String] = inputQuery.split("\\r?\\n")    //Split based on the newline character
@@ -48,75 +126,76 @@ object MainTester {
 
       )
     })
-
-    /*
-      For Q2
-      Tabs Analysis:
-       -  tab0 -> As X -> GraduateStudent ->    Cost: 1379623
-       -  tab1 -> As Y -> University  ->        Cost: 724685
-       -  tab2 -> As Z -> Department  ->        Cost: 309815
-       -  tab3 -> AS X, AS Z -> memberOf ->     Cost: 5580609
-       -  tab4 -> AS Z, AS Y -> suborganOf->    Cost: 160140
-       -  tab5 -> AS X, AS Y -> undergraDegree->Cost: 1619476
-     */
-
-    //    WHERE tab4.Z=tab2.Z AND tab1.Y=tab4.Y AND tab4.Y=tab5.Y AND tab3.Z=tab4.Z AND tab5.X=tab0.X AND tab0.X=tab3.X
-    //    We essentially want to find the Hamiltonial path  https://www.hackerearth.com/practice/algorithms/graphs/hamiltonian-path/tutorial/
-
-    //    When there are N nodes, there are N - 1 directed edges that can lead from it (going to every other node)
-    //  https://stackoverflow.com/questions/5058406/what-is-the-maximum-number-of-edges-in-a-directed-graph-with-n-nodes?fbclid=IwAR0EePHHzvtL1b0XtckIbjjaXhGaYG7HEgyNDEd5EMF34dKdIv9Oiz_zYok
-    //  PROOF: https://www.quora.com/How-do-I-prove-that-the-minimum-number-of-edges-in-a-connected-graph-with-n-vertices-is-n-1
-
-    /*
-//    Q2 data
-//    var costMap: HashMap[String, Double] = HashMap("tab0" -> 1379623, "tab1" -> 724685, "tab2" -> 309815, "tab3" -> 5580609, "tab4" -> 160140, "tab5" -> 1619476)
 //
-//    var fullMap: HashMap[String, Array[String]] = HashMap("X"-> Array("tab0", "tab3", "tab5"), "Y"-> Array("tab1","tab4", "tab5"), "Z"->Array("tab2","tab3","tab4"))
-
-//    Q4 Data
-//    var costMap: HashMap[String, Double] = HashMap("tab0" -> 1379623, "tab1" -> 724685, "tab2" -> 309815, "tab3" -> 5580609, "tab4" -> 160140)
+//    /*
+//      For Q2
+//      Tabs Analysis:
+//       -  tab0 -> As X -> GraduateStudent ->    Cost: 1379623
+//       -  tab1 -> As Y -> University  ->        Cost: 724685
+//       -  tab2 -> As Z -> Department  ->        Cost: 309815
+//       -  tab3 -> AS X, AS Z -> memberOf ->     Cost: 5580609
+//       -  tab4 -> AS Z, AS Y -> suborganOf->    Cost: 160140
+//       -  tab5 -> AS X, AS Y -> undergraDegree->Cost: 1619476
+//     */
 //
-//    var fullMap: HashMap[String, Array[String]] = HashMap("X"-> Array("tab0", "tab1", "tab2", "tab3", "tab4"), "Y1" -> Array("tab2"), "Y2" -> Array("tab3"), "Y3" -> Array("tab4"))
-*/
+//    //    WHERE tab4.Z=tab2.Z AND tab1.Y=tab4.Y AND tab4.Y=tab5.Y AND tab3.Z=tab4.Z AND tab5.X=tab0.X AND tab0.X=tab3.X
+//    //    We essentially want to find the Hamiltonial path  https://www.hackerearth.com/practice/algorithms/graphs/hamiltonian-path/tutorial/
+//
+//    //    When there are N nodes, there are N - 1 directed edges that can lead from it (going to every other node)
+//    //  https://stackoverflow.com/questions/5058406/what-is-the-maximum-number-of-edges-in-a-directed-graph-with-n-nodes?fbclid=IwAR0EePHHzvtL1b0XtckIbjjaXhGaYG7HEgyNDEd5EMF34dKdIv9Oiz_zYok
+//    //  PROOF: https://www.quora.com/How-do-I-prove-that-the-minimum-number-of-edges-in-a-connected-graph-with-n-vertices-is-n-1
+//
+//    /*
+////    Q2 data
+////    var costMap: HashMap[String, Double] = HashMap("tab0" -> 1379623, "tab1" -> 724685, "tab2" -> 309815, "tab3" -> 5580609, "tab4" -> 160140, "tab5" -> 1619476)
+////
+////    var fullMap: HashMap[String, Array[String]] = HashMap("X"-> Array("tab0", "tab3", "tab5"), "Y"-> Array("tab1","tab4", "tab5"), "Z"->Array("tab2","tab3","tab4"))
+//
+////    Q4 Data
+////    var costMap: HashMap[String, Double] = HashMap("tab0" -> 1379623, "tab1" -> 724685, "tab2" -> 309815, "tab3" -> 5580609, "tab4" -> 160140)
+////
+////    var fullMap: HashMap[String, Array[String]] = HashMap("X"-> Array("tab0", "tab1", "tab2", "tab3", "tab4"), "Y1" -> Array("tab2"), "Y2" -> Array("tab3"), "Y3" -> Array("tab4"))
+//*/
+//
+//    //TODO fullmap and costmap the most important here
+//
+//    var bestComb : List[(String, Double)] = Nil // A list to preserve the best combinations
+//
+//    //First iterate over the Hashmap (key,values). Values is a list that contains all the available tables between them
+//    //We are certain that there are no joins where only one table of a specific key exists
+//    for (dictIter <- fullMap; if dictIter._2.length > 1 ){
+////      println("\n\nFor the key " + dictIter._1)
+//
+//      var tempComb : List[(String, Double)] = Nil
+//      for(innerList <- dictIter._2.indices){  //  Iterate over the list elements. innerList is an index value
+////        println("Iterate over list elements " +dictIter._2(innerList))
+//        for(remElem <- innerList+1 until dictIter._2.length){   // Go from the next index to the end of the table to track all combinations
+////          println("Remaining elements " +dictIter._2(remElem))
+//
+//          val tempProd: Double = costMap(dictIter._2(innerList)) * costMap(dictIter._2(remElem))  // Find the cost of join between two arrays, by calculating the product
+////          println("\n\nCost of element "+ dictIter._2(innerList) +" is "+costMap(dictIter._2(innerList)))
+////          println("Cost of element "+ dictIter._2(remElem) +" is "+costMap(dictIter._2(remElem)))
+//          val tempTuple: (String, Double) = (dictIter._2(innerList)+"."+dictIter._1+"="+dictIter._2(remElem)+"."+dictIter._1, tempProd)  // Create a temporary tuple with the table name and the cost
+////          println("Temporary tuple", tempTuple)
+//          tempComb = tempTuple :: tempComb
+//        }
+//      }
+//
+////      println("List with all the combinations \t"+ tempComb)
+//      val sortedtempComb : List[(String, Double)]= tempComb.sortBy(_._2)  //Sort everything by the join cost(ascending)
+//      //      println("List with SORTED the combinations \t"+ sortedtempComb)
+//
+//      for(bestElem <- 0 until dictIter._2.length - 1){ //Keep the N - 1 elements that we want from each case. NOTE N-1 from the initial list with the elements
+//        bestComb = sortedtempComb(bestElem) :: bestComb
+//      }
+//
+//    }
+//
+//    val sortedBestComb : List[String] = bestComb.sortBy(_._2).map(x => x._1) //Sort once again to get the final ordering, and keep the only the combinations
+//    val finalWhereQuery = " WHERE " +sortedBestComb.mkString(" AND ") //Final WHERE STATEMENT OPTIMIZED
+//    println("Final list with SORTED best combinations "+ finalWhereQuery)
 
-    var bestComb : List[(String, Double)] = Nil // A list to preserve the best combinations
-
-    //First iterate over the Hashmap (key,values). Values is a list that contains all the available tables between them
-    //We are certain that there are no joins where only one table of a specific key exists
-    for (dictIter <- fullMap; if dictIter._2.length > 1 ){
-//      println("\n\nFor the key " + dictIter._1)
-
-      var tempComb : List[(String, Double)] = Nil
-      for(innerList <- dictIter._2.indices){  //  Iterate over the list elements. innerList is an index value
-//        println("Iterate over list elements " +dictIter._2(innerList))
-        for(remElem <- innerList+1 until dictIter._2.length){   // Go from the next index to the end of the table to track all combinations
-//          println("Remaining elements " +dictIter._2(remElem))
-
-          val tempProd: Double = costMap(dictIter._2(innerList)) * costMap(dictIter._2(remElem))  // Find the cost of join between two arrays, by calculating the product
-//          println("\n\nCost of element "+ dictIter._2(innerList) +" is "+costMap(dictIter._2(innerList)))
-//          println("Cost of element "+ dictIter._2(remElem) +" is "+costMap(dictIter._2(remElem)))
-          val tempTuple: (String, Double) = (dictIter._2(innerList)+"."+dictIter._1+"="+dictIter._2(remElem)+"."+dictIter._1, tempProd)  // Create a temporary tuple with the table name and the cost
-//          println("Temporary tuple", tempTuple)
-          tempComb = tempTuple :: tempComb
-        }
-      }
-
-//      println("List with all the combinations \t"+ tempComb)
-      val sortedtempComb : List[(String, Double)]= tempComb.sortBy(_._2)  //Sort everything by the join cost(ascending)
-      //      println("List with SORTED the combinations \t"+ sortedtempComb)
-
-      for(bestElem <- 0 until dictIter._2.length - 1){ //Keep the N - 1 elements that we want from each case. NOTE N-1 from the initial list with the elements
-        //        println(sortedtempComb(bestElem))
-        bestComb = sortedtempComb(bestElem) :: bestComb
-      }
-
-    }
-
-    //    println("Final list with best combinations "+ bestComb)
-    //    val sortedBestComb = bestComb.sortBy(_._2)
-    val sortedBestComb : List[String] = bestComb.sortBy(_._2).map(x => x._1) //Sort once again to get the final ordering, and keep the only the combinations
-    val finalWhereQuery = " WHERE " +sortedBestComb.mkString(" AND ") //Final WHERE STATEMENT OPTIMIZED
-    println("Final list with SORTED best combinations "+ finalWhereQuery)
+    val finalWhereQuery = QueryOptimizerLogic(fullMap, costMap)
 
     val finalQuery = finalSelectQuery + finalFromQuery + finalWhereQuery
     newlineSplit(1) = finalQuery
